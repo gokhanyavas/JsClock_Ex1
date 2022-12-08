8e6711c9ed08391744bff54a6f7399bb3b86f428
Gokhan Yavas <yvsgkhn@gmail.com>
2022-12-08 22:23:12 +0300

    js clock example first commit

---
+++ js/clock.js
@@ -0,0 +1,34 @@
+let name = prompt("Kullanici Adiniz : ")
+let myName = document.querySelector("#myName")
+
+myName.innerHTML = `${name}`
+
+console.log(myName)
+
+function showTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let days = [
+        "Pazar",
+        "Pazartesi",
+        "Salı",
+        "Çarşamba",
+        "Perşembe",
+        "Cuma",
+        "Cumartesi",
+      ];
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('myClock').innerHTML =  h + ":" + m + ":" + s + " "+ days[today.getDay()];
+    setTimeout(showTime, 1000);
+
+  }
+
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+  }
+
+  showTime()
